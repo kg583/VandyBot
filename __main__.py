4451b280eb05b495efea72c3edaac4a186751f10
Kevin Gomez <kgscience@hotmail.com>
2020-09-14 13:24:37 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,24 +1,10 @@
 import asyncio
 from vandybot import main
 
-# Debug flag
-DEBUG = False
-
-
-async def debug():
-    # Left as pass while not being used
-    pass
-
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    if not DEBUG:
-        try:
-            loop.run_until_complete(main())
-        except KeyboardInterrupt:
-            loop.close()
-    else:
-        try:
-            loop.run_until_complete(debug())
-        except KeyboardInterrupt:
-            loop.close()
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        loop.close()
