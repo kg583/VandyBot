87778195f9229b1990060e247cbd1c81e0702997
Kevin Gomez <kgscience@hotmail.com>
2021-02-04 00:47:32 -0600

    Updated COVID dashboard

--- vandybot/covid/__init__.py
+++ vandybot/covid/__init__.py
@@ -21,7 +21,7 @@ class Covid(commands.Cog):
         for index, header in enumerate(headers):
             embed.add_field(name=header, value="\n".join(row[index] for row in rows))
 
-        embed.set_footer(text="Spring semester testing data is not yet available")
+        embed.set_footer(text="Symptomatic test results are not yet fully disclosed")
         return embed
 
     @staticmethod
@@ -32,15 +32,15 @@ class Covid(commands.Cog):
         response = await fetch(self._session, self.URL)
         soup = BeautifulSoup(response, "html.parser")
         rows = soup.find("table").find_all("tr")
-        entries = [[entry.get_text() for entry in row.find_all("td")] for row in rows][1:][::-1]
+        entries = [[entry.get_text().split(" ")[0] for entry in row.find_all("td")] for row in rows][1:][::-1]
 
         # Calculate total
-        total = ["TOTAL", self.sum_column(entries, 1), self.sum_column(entries, 2)]
-        total.append(f"{100 * total[2] / total[1]:.2f}%")
-        total = list(map(bold, [total[0], f"{total[1]:,}", f"{total[2]:,}", str(total[3])]))
+        total = ["1/10/21", "Present", self.sum_column(entries, 3), self.sum_column(entries, 4)]
+        total.append(f"{100 * total[3] / total[2]:.2f}%")
+        total = list(map(bold, [total[0], total[1], "", f"{total[2]:,}", f"{total[3]:,}", total[4]]))
         entries.append(total)
 
-        return [[entry[0], f"{entry[2]}/{entry[1]}", entry[3]] for entry in entries]
+        return [[f"{entry[0]} - {entry[1]}", f"{entry[4]}/{entry[3]}", entry[5]] for entry in entries]
 
     async def startup(self):
         pass
@@ -55,6 +55,6 @@ class Covid(commands.Cog):
                       usage="")
     async def covid(self, ctx):
         data = await self.get_data()
-        headers = tuple(map(underline, ("Week", "Test Results", "Positivity Rate")))
+        headers = tuple(map(underline, ("Period", "Asymp. Positives", "Asymp. Positivity")))
         embed = self.generate_embed(title="COVID-19 Dashboard", url=self.URL, headers=headers, rows=data)
         await ctx.send(embed=embed)
